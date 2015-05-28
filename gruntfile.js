12ab8842869e322a74c3ee98c5c481fcf66f1323
Johannes Fischer <johannes@nanameue.jp>
2015-05-28 18:43:15 +0900

    grunt minify css

--- gruntfile.js
+++ gruntfile.js
@@ -1,18 +1,34 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      concat: {
-        dist: {
-          src: [
-            'assets/bower_components/normalize.css/normalize.css',
-            'assets/css/dev/styles.css'
-          ],
-          dest: 'assets/css/build/styles.css'
-        }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: [
+          'assets/bower_components/normalize.css/normalize.css',
+          'assets/css/dev/styles.css'
+        ],
+        dest: 'assets/css/build/styles.css'
       }
-    });
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat']);
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'assets/css/build',
+          src: ['*.css', '!*.min.css'],
+          dest: 'assets/css/build',
+          ext: '.min.css'
+        }]
+      }
+    }
+  });
+  // 3. Where we tell Grunt we plan to use this plug-in.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+  grunt.registerTask('default', ['concat', 'cssmin']);
 };
