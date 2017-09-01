fa02367225616d59d4355b26c0d506aefa768bbb
Johannes Fischer <johannes.fischer@rakuten.com>
2017-09-01 12:32:44 +0900

    add webfont loader

--- assets/source/js/script.js
+++ assets/source/js/script.js
@@ -1,2 +1,14 @@
 // Put JS code here
 console.info('Add your JS code to "assets/source/js"');
+
+var fontRegular = new FontFaceObserver('Dosis', {
+  weight: 400
+});
+var fontBold = new FontFaceObserver('Dosis', {
+  weight: 700
+});
+
+Promise.all([fontRegular.load(), fontBold.load()]).then(function () {
+  console.log('Family A & B have loaded');
+  document.body.classList.add('webfont');
+});
