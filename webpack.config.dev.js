0559dec7064121b77ca49cafdae00c9631690b71
Johannes Fischer <git@johannes-fischer.de>
2021-10-06 16:36:08 +0900

    Meaningful update

--- webpack.config.dev.js
+++
@@ -1,7 +0,0 @@
-require('babel-core/register');
-
-module.exports = require('./webpack.config').default({
-  build: true,
-  dev: true,
-  watch: true
-});
--- webpack.config.prod.js
+++
@@ -1,5 +0,0 @@
-require('babel-core/register');
-
-module.exports = require('./webpack.config').default({
-  build: true
-});
